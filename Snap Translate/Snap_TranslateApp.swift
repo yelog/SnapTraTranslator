8db8b11f329532dec04d106abc3f4ab16cfbe38d
yelog <yelogeek@gmail.com>
2026-01-12 14:52:20 +0800

    Initial Commit

---
+++ Snap Translate/Snap_TranslateApp.swift
@@ -0,0 +1,17 @@
+//
+//  Snap_TranslateApp.swift
+//  Snap Translate
+//
+//  Created by 杨玉杰 on 2026/1/12.
+//
+
+import SwiftUI
+
+@main
+struct Snap_TranslateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
